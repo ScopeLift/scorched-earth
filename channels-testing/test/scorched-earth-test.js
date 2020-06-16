475d0dd631c515c137b062d462e97e28ffd8403a
Ben DiFrancesco <ben@scopelift.co>
2020-06-16 15:10:17 -0400

    Set up test harness for the ScorchedEarth contract

---
+++ channels-testing/test/scorched-earth-test.js
@@ -0,0 +1,18 @@
+const { accounts, contract, web3 } = require('@openzeppelin/test-environment');
+const { expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+const { expect } = require('chai');
+
+const ScorchedEarth = contract.fromArtifact('ScorchedEarth');
+
+describe('ScorchedEarth', () => {
+    const [ deployer ] = accounts;
+
+    before(async ()=> {
+        this.instance = await ScorchedEarth.new({from: deployer});
+    });
+
+    it('should see the deployed ScorchedEarth contract', async () => {
+        expect(this.instance.address.startsWith('0x')).to.be.true;
+        expect(this.instance.address.length).to.equal(42);
+    });
+});
