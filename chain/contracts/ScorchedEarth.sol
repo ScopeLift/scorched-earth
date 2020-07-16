65c68f40fed671f65eec19aa47c6bee68083deee
Ben DiFrancesco <ben@scopelift.co>
2020-07-16 07:02:49 -0400

    Stub contract & setup compilation

---
+++ chain/contracts/ScorchedEarth.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.8;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
+
+contract ScorchedEarth {
+
+}
