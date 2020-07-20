fdfd99ce6bfeb96b7463dd60f78373efac04e5ac
Ben DiFrancesco <ben@scopelift.co>
2020-07-20 07:33:41 -0400

    Stub the ForceMoveApp validTransistion implementation

--- chain/contracts/ScorchedEarth.sol
+++ chain/contracts/ScorchedEarth.sol
@@ -2,8 +2,21 @@
 pragma solidity ^0.6.8;
 pragma experimental ABIEncoderV2;
 
+import '@statechannels/nitro-protocol/contracts/interfaces/ForceMoveApp.sol';
+import '@statechannels/nitro-protocol/contracts/Outcome.sol';
 import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 
-contract ScorchedEarth {
+contract ScorchedEarth is ForceMoveApp {
+    using SafeMath for uint256;
 
+    constructor () public { }
+
+    function validTransition(
+        VariablePart memory _fromPart,
+        VariablePart memory _toPart,
+        uint48, // turnNumB
+        uint256  // nParticipants
+    ) public pure override returns (bool) {
+        return true;
+    }
 }
